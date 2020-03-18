18bce5bd9336dbfc72bf70a04b07a40774a41c94
Shashwat Chavan <spchavan@outlook.com>
2020-03-17 20:30:19 -0400

    Updated function
    Updated return function to create better message

--- Python_Basic_Projects/BMI_Calculator_Function.py
+++ Python_Basic_Projects/BMI_Calculator_Function.py
@@ -15,12 +15,10 @@ Weight_LB3 =150
 
 def BMI_Calcualtor(name,Height_in,Weight_LB):
     BMI=703 *(Weight_LB/(Height_in**2))
-    print("BMI: ")
-    print(BMI)
     if BMI<25:
-        return name +" not overweight"
+        return  "BMI for "+name+"is: "+ str(BMI)+" so "+ name +" not overweight"
     else:
-        return name +" is overweight"
+        return  "BMI for "+name+"is: "+ str(BMI)+" so "+ name +" is overweight"
 
 Person1_BMI=BMI_Calcualtor(PersonName1,Height_in1,Weight_LB1)
 Person2_BMI=BMI_Calcualtor(PersonName2,Height_in2,Weight_LB2)
@@ -29,5 +27,3 @@ Person3_BMI=BMI_Calcualtor(PersonName3,Height_in3,Weight_LB3)
 print(Person1_BMI)
 print(Person2_BMI)
 print(Person3_BMI)
-
-
